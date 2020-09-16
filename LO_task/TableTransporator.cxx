2bcd08d49c283c95d0c238e4a3761b34e7e0ccd7
Никита Никифоров <nickiforov.nik@gmail.com>
2020-09-16 17:09:16 +0300

    Add first table and text in document

--- LO_task/TableTransporator.cxx
+++ LO_task/TableTransporator.cxx
@@ -14,7 +14,14 @@
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/frame/XComponentLoader.hpp>
 #include <com/sun/star/lang/XMultiComponentFactory.hpp>
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/registry/XSimpleRegistry.hpp>
+
+#include <com/sun/star/text/XTextDocument.hpp>
+#include <com/sun/star/text/XText.hpp>
+
+#include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/text/XTextContent.hpp>
 
 #include <string.h>
 
@@ -24,6 +31,7 @@ using namespace com::sun::star::beans;
 using namespace com::sun::star::bridge;
 using namespace com::sun::star::frame;
 using namespace com::sun::star::registry;
+using namespace com::sun::star::text;
 
 using ::rtl::OUString;
 using ::rtl::OUStringToOString;
@@ -58,7 +66,17 @@ SAL_IMPLEMENT_MAIN() {
             xDesktop2->loadComponentFromURL(OUString("private:factory/swriter"), // URL to the ods file
                                             OUString( "_blank" ), 0,
                                             Sequence < ::com::sun::star::beans::PropertyValue >());
+        Reference <XTextDocument> xTextDocument(xComponent, UNO_QUERY);
+        Reference <XText> xText = xTextDocument->getText();
+        xText->setString(OUString::createFromAscii("Hello, world! This is my first table!"));
         
+        Reference <XMultiServiceFactory> oDocMSF(xTextDocument, UNO_QUERY);
+        Reference <XTextTable> xTable(oDocMSF->createInstance(OUString::createFromAscii("com.sun.star.text.TextTable")), UNO_QUERY);
+        xTable->initialize(4, 4);
+        auto xTextRange = xText->getEnd();
+    
+        Reference <XTextContent> xTextContent (xTable, UNO_QUERY);
+        xText->insertTextContent(xTextRange, xTextContent, (unsigned char) 0);
     }
     catch (::cppu::BootstrapException& e) {
         fprintf(stderr, "caught BootstrapException: %s\n",
