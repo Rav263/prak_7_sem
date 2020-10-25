65565498a8544a3fc17be28449ade4396ebf0d9a
Никита Никифоров <nickiforov.nik@gmail.com>
2020-10-25 19:03:35 +0300

    Add first tests and update build system

--- IO_task/src/main.cpp
+++ IO_task/src/main.cpp
@@ -2,21 +2,42 @@
 #include <vector>
 
 #include "temperature.hpp"
+#include "solution.hpp"
 
 int main() {
-    std::vector<Temp *> temps= {new TempFirst(9), new TempSecond(9), new TempThird(9)};
+    uint32_t num_of_procs = 1, num_of_problems = 0;
 
-    for (auto now : temps) {
-        now->decrease_temp();
+    std::cout << "Enter num of procs: ";
+    std::cin >> num_of_procs;
+
+    std::cout << "Enter num of problems: ";
+    std::cin >> num_of_problems;
+
+    BigSolution *init_solution = new BigSolution(num_of_procs, num_of_problems);
+
+    for (uint32_t i = 0; i < num_of_problems; i++) {
+        Problem *now = new Problem(2.0, 1, i);
+
+        init_solution->add_new_problem(now);
     }
 
-    for (auto now : temps) {
-        std::cout << now->temp << " ";
-    }
+    Solution *solution = init_solution;
+    solution->get_problem(0)->add_next_link(solution->get_problem(1));
+    
+    std::cout << "INIT SOLUTION: " << std::endl;
+    solution->print_solution();
+    
+    Solution *new_solution = solution->copy_solution();
+    new_solution->get_problem(1)->change_proc_index(2);
+    
+    std::cout << std::endl << "AFTER CHANGE INIT SOLUTION" << std::endl;
+    solution->print_solution();
+    
+    std::cout << std::endl << "NEW SOLUTION" << std::endl;
+    new_solution->print_solution();
 
-    std::cout << std::endl;
-
-    for (auto now : temps) {
-        delete now;
-    }
+    delete solution;
+    std::cout << std::endl << "NEW SOLUTION AFTER DELETE" << std::endl;
+    new_solution->print_solution();
+    delete new_solution; 
 }
